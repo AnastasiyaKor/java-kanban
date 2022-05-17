2ee3c29eb69c6a9b464e1af52f19c08ec155cc9f
AnastasiyaK <stasykorchagina@yandex.ru>
2022-05-17 17:47:45 +0500

    refactor: (спринт4) Добавлен новый класс

--- src/HistoryManager.java
+++ src/HistoryManager.java
@@ -3,6 +3,5 @@ import java.util.List;
 public interface HistoryManager {
 
     void add(Task task);//помачает задачи, как просмотренные
-
     List<Task> getHistory();//возвращает задачи в список
 }
